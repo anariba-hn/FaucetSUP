e8d5baf9ac29c74e822274161a3bd63b0275c7db
Dennis Anariba <anariba.hn@hotmail.com>
2018-07-13 17:21:07 -0600

    Improving LougOut call

--- admincenter/logout.php
+++ admincenter/logout.php
@@ -1,4 +1,7 @@
 <?php
 session_destroy();
 header("Location: ../admincenter/index.html");
+$response['status'] = 200;
+header('Content-type: application/json; charset=utf8');
+echo json_encode($response);
 ?>
