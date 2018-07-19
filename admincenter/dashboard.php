50461b58ec7c372f40cada97b160ccf38c35ce50
Dennis Anariba <anariba.hn@hotmail.com>
2018-07-19 16:27:15 -0600

    Dynamic Table on Dashboard3.0

--- admincenter/dashboard.php
+++ admincenter/dashboard.php
@@ -14,6 +14,8 @@ if(empty($_SESSION['admin']))
     <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="../css/adminstylee.css">
         <link rel="stylesheet" href="../css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.css">
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
         <!-- SCRIPTS -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -162,8 +164,8 @@ if(empty($_SESSION['admin']))
     <!--SCRIPTS-->
     <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
     <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
     <script src="../js/admin.js"></script>
-    <script src="../js/bootstrap.bundle.js"></script></body>
+    <script src="../js/bootstrap.bundle.js"></script>
 </html>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
