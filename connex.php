9f8e652f5c4698bf647736adda9a92377871b093
Dennis Anariba <anariba.hn@hotmail.com>
2018-07-21 13:54:30 -0600

    updated connex parms

--- connex.php
+++ connex.php
@@ -3,7 +3,7 @@ ini_set('display_errors', true);
 //Connection Variables
 $host = "localhost";
 $user = "root";
-$password = "superior2020";
+$password = "mike123!";
 $db = "superiorf";
 
 //Set connection to MySQL
