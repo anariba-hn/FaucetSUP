f9d12f5bc39d0c7e0b3256d77394d4a18aa139b1
Dennis Anariba <anariba.hn@hotmail.com>
2018-08-09 16:39:17 -0600

    Updated Verification process

--- getUser.php
+++ getUser.php
@@ -7,19 +7,28 @@ include("./connex.php");
 
 	if($user_address != null)
 	{
-		$query = "SELECT * FROM users WHERE user_email = '$user_address' or user_address = '$user_address' AND active = '1'";
-		if (!$result = mysqli_query($cnn, $query))
-        exit(mysqli_error($cnn));
-	    if(mysqli_num_rows($result) > 0)
-	    {
-	    	while($row = mysqli_fetch_assoc($result))
-	    	{
-	    		$response = $row;
-	    	}
-	    }else{
-	    		$response['status'] = 404;
-				$response['message'] = "Invalid Request !";	
-	    }
+		$sql = "SELECT user_email, user_address, active FROM users WHERE active = '1' AND user_email = '$user_address' OR user_address = '$user_address'";
+		if(!$result = mysqli_query($cnn, $sql))
+			exit(mysqli_error($cnn));
+		if(mysqli_num_rows($result) > 0)
+		{
+			$query = "SELECT * FROM users WHERE user_email = '$user_address' or user_address = '$user_address'";
+			if (!$result = mysqli_query($cnn, $query))
+	        	exit(mysqli_error($cnn));
+		    if(mysqli_num_rows($result) > 0)
+		    {
+		    	while($row = mysqli_fetch_assoc($result))
+		    	{
+		    		$response = $row;
+		    	}
+		    }else{
+		    		$response['status'] = 404;
+					$response['message'] = "Invalid Request !";	
+		    }
+		}else{
+			$response['status'] = 404;
+			$response['message'] = "Invalid Request !";	
+		}
 
 	    header('Content-type: application/json; charset=utf8');
     	echo json_encode($response);
